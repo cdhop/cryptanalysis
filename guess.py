1702a022c8a6d380063155074188479b9fb3ae4f
root <root@localhost.localdomain>
2017-04-02 12:56:26 -0600

    Added solves folder

--- guess.py
+++ guess.py
@@ -15,16 +15,23 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         key_string = sys.argv[2]
         key_dict = json.loads(key_string)
-        sorted_key_list = sorted(key_dict.items(), key=itemgetter(1))
+        sorted_key_list = sorted(key_dict.items(), key=itemgetter(0))
         cipher_text = sys.argv[1]
         
         plain_text = guess(cipher_text, key_dict)
+        key_applied = ''
+        for c in cipher_text:
+            if c in key_dict.keys():
+                key_applied += '+'
+            else:
+                key_applied += '-'
 
         print("Cipher Text: ") 
         print(cipher_text)
         print('-')
         print("Guess Text: ") 
         print(plain_text)
+        print(key_applied)
         print('-')
         print("Sorted Key: ")
         for element in sorted_key_list:
